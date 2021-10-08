da96ed898dce053ca14002dead74af4337efac8c
bon <nsittee@gmail.com>
2021-10-08 20:24:45 +0700

    Add theme to paper provider

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-/**
- * @format
- */
+import React from 'react'
+import App from './src/App'
+import { AppRegistry } from 'react-native'
+import { name as appName } from './app.json'
+import { Provider as PaperProvider, DefaultTheme } from 'react-native-paper'
 
-import { AppRegistry } from 'react-native';
-import App from './src/App';
-import { name as appName } from './app.json';
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    accent: 'yellow',
+  },
+}
 
-AppRegistry.registerComponent(appName, () => App);
+export default function Main() {
+  return (
+    <PaperProvider theme={theme}>
+      <App />
+    </PaperProvider >
+  )
+}
+
+AppRegistry.registerComponent(appName, () => Main);
