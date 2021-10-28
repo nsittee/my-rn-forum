1c1d4d803545f2c27fcd8a89399e8792debe8e72
bon <nsittee@gmail.com>
2021-10-28 12:44:36 +0700

    Add simple working stack-navigation

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
-import React from 'react'
+// import 'react-native-gesture-handler'
+import * as React from 'react';
 import App from './src/App'
 import { AppRegistry } from 'react-native'
 import { name as appName } from './app.json'
-import { Provider as PaperProvider, DefaultTheme } from 'react-native-paper'
-
-const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: 'tomato',
-    accent: 'yellow',
-  },
-}
+import { NavigationContainer } from '@react-navigation/native';
+// import { Provider as PaperProvider, DefaultTheme } from 'react-native-paper'
 
 export default function Main() {
   return (
-    <PaperProvider theme={theme}>
+    <NavigationContainer>
       <App />
-    </PaperProvider >
-  )
+    </NavigationContainer>
+  );
 }
 
 AppRegistry.registerComponent(appName, () => Main);
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,67 @@
-import React from 'react'
-import { MainPage } from './screen/MainPage'
+import * as React from 'react'
+import { Button, View } from 'react-native'
+import { createStackNavigator, StackScreenProps } from '@react-navigation/stack'
 
-const App = () => {
+type Props = StackScreenProps<any>
+
+function HomeScreen({ navigation }: Props) {
   return (
-    <MainPage />
-  )
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Go to Profile"
+        onPress={() => navigation.navigate('Profile')}
+      />
+    </View>
+  );
 }
 
-export default App
+function ProfileScreen({ navigation }: Props) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Go to Notifications"
+        onPress={() => navigation.navigate('Notifications')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+function NotificationsScreen({ navigation }: Props) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Go to Settings"
+        onPress={() => navigation.navigate('Settings')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+function SettingsScreen({ navigation }: Props) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Notifications" component={NotificationsScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Settings" component={SettingsScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <MyStack />
+  );
+}
